bfec5c1f79e6cd7442afe3c61fbd1dc67c1fa304
Randall Valenciano Fallas <ravf.226@gmail.com>
2016-06-27 19:01:57 -0600

    multiple route markers and delete

--- www/js/app.js
+++ www/js/app.js
@@ -73,7 +73,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
 }).controller('MapRouteCtrl', function($scope, $state, $cordovaGeolocation) {
   var options = {timeout: 10000, enableHighAccuracy: true};
 
-
+  var waypts = {};
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
 
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -89,26 +89,68 @@ angular.module('starter', ['ionic', 'ngCordova'])
     //Wait until the map is loaded
     var directionsDisplay = new google.maps.DirectionsRenderer();
     var directionsService = new google.maps.DirectionsService();
-    var start = new google.maps.LatLng(37.334818, -121.884886);
+    var start = new google.maps.LatLng(9.9530643,-84.1104048);
     //var end = new google.maps.LatLng(38.334818, -181.884886);
-    var end = new google.maps.LatLng(37.441883, -122.143019);
+    var end = new google.maps.LatLng(9.9604673,-84.0784428);
     var bounds = new google.maps.LatLngBounds();
     bounds.extend(start);
     bounds.extend(end);
     $scope.map.fitBounds(bounds);
-    var request = {
-        origin: start,
-        destination: end,
-        travelMode: google.maps.TravelMode.DRIVING
-    };
-    directionsService.route(request, function (response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-            directionsDisplay.setMap($scope.map);
-        } else {
-            alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
-        }
+    calculateRoute();
+
+    google.maps.event.addListener($scope.map, 'click', function(evt){
+      // we add a marker and push in waypoints array
+      var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: evt.latLng,
+          draggable: true
+      });
+      var markerId = getMarkerUniqueId(evt.latLng.lat(), evt.latLng.lng());
+      marker.addListener('click', function() {
+
+      //  for (var i=0, iLen=waypts.length; i<iLen; i++) {
+      //    if (waypts[i].location.equals(marker.position)) {
+      //      waypts.splice(i,1);
+            //delete waypts[i];
+      //      break;
+      //    }
+      //  }
+        delete waypts[markerId];
+        calculateRoute();
+        marker.setMap(null);
+      });
+      waypts[markerId] = {
+        location: evt.latLng,
+        stopover: false
+      };
+      calculateRoute();
     });
+
+    function calculateRoute() {
+      var dataArray = new Array;
+      for(var o in waypts) {
+        dataArray.push(waypts[o]);
+     }
+      var request = {
+          origin: start,
+          destination: end,
+          waypoints: dataArray,
+          optimizeWaypoints: true,
+          travelMode: google.maps.TravelMode.DRIVING
+      };
+      directionsService.route(request, function (response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+              directionsDisplay.setDirections(response);
+              directionsDisplay.setMap($scope.map);
+          } else {
+              alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+          }
+      });
+    }
+    var getMarkerUniqueId= function(lat, lng) {
+      return lat + '_' + lng;
+    }
 
   }, function(error){
     console.log("Could not get location");
